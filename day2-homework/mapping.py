9d1d518070e356ae2354f3a93d6442c2465733ac
Katherine Bossone <kbosson1@jhu.edu>
2019-08-27 19:19:06 -0400

    fixed results

--- day2-homework/mapping.py
+++ day2-homework/mapping.py
@@ -7,15 +7,15 @@ genes_of_interest = {}
 
 for line in open(sys.argv[1]):
     col = line.rstrip("\n").split("\t")
-    genes_of_interest[col[0]] = col[1]
+    genes_of_interest[col[0].rstrip(" ")] = col[1]
 
 for line in open(sys.argv[2]):
     col = line.rstrip("\n").split("\t")
     gene_name = col[8]
     if gene_name in genes_of_interest:
         result = genes_of_interest[gene_name]
-        print(result, "\t", line)
+        print(result + "\t" + line)
     elif sys.argv[3] == "bad":
-        print("Bad input", "\t", line)
+        print("Bad input" + "\t" + line)
     else:
         continue
