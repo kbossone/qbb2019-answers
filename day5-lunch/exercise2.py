a72ddf6cb49007fe74305eb38d27d45d5c1d8d90
Katherine Bossone <kbosson1@jhu.edu>
2019-08-30 13:31:59 -0400

    exercise 2

--- day5-lunch/exercise2.py
+++ day5-lunch/exercise2.py
@@ -5,3 +5,33 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+
+#~/qbb2019-answers/results/stringtie/SRR072893/t_data.ctab
+# need chromosome[1] start[3] end[4] name[5]
+# start end + 500
+
+
+df = pd.read_csv(open(sys.argv[1]), sep="\t")
+
+genes=df.loc[:,(("t_name", "start","end","chr","strand"))]
+
+for i,t in genes.iterrows():
+    if t.loc["strand"] == "+":
+        prom1 = int(t.loc["start"]) + 500
+        prom2 = int(t.loc["start"]) - 500
+        a = t.loc["chr"]
+        t_name = t.loc["t_name"]
+        if prom2 > 0: 
+            print(a,prom1,prom2,t_name,sep="\t")
+        else:
+            print(a,prom1,t.loc["start"],t_name,sep="\t")
+
+    elif t.loc["strand"] == "-":
+        prom3 = int(t.loc["end"]) + 500
+        prom4 = int(t.loc["end"]) - 500
+        a = t.loc["chr"]
+        t_name = t.loc["t_name"]
+        if prom4 > 0:
+            print(a,prom3,prom4,t_name,sep="\t")
+        else:
+            print(a,prom3,t.loc["start"],t_name,sep="\t")
