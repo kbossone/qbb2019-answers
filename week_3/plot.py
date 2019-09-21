affb98fdeef9cd36b74bc99598f83ce99e6d04ce
Katherine Bossone <kbosson1@jhu.edu>
2019-09-20 22:40:56 -0400

    Week3

--- week_3/plot.py
+++ week_3/plot.py
@@ -32,34 +32,39 @@ for line in snp:
         ai[a] += 1
     else:
         ai[a] = 1
-
     
     
     
-fig, ax = plt.subplots(4)
+fig,((ax1, ax2),(ax3,ax4)) = plt.subplots(nrows=2,ncols=2)
 
-ax[0].hist(read_depth,bins = 100,density = True)
-ax[0].set_title("Read Depth of SNPs")
-ax[0].set_xlabel("Read Depth")
-ax[0].set_ylabel("Postion of SNP")
+ax1.hist(read_depth,bins = 100,density = True)
+ax1.set_title("Read Depth of SNPs",size=15)
+ax1.set_xlabel("Read Depth")
+for tick in ax1.get_xticklabels():
+    tick.set_rotation(90)
+    tick.set_size(5)
+ax1.set_ylabel("Postion of SNP")
 
-ax[1].hist(quality, bins = 50,density = True, range= (0,2500))
-ax[1].set_title("Read Quality of SNPs")
-ax[1].set_xlabel("Read Quality")
-ax[1].set_ylabel("Postion of SNP")
+ax2.hist(quality, bins = 50,density = True, range= (0,2500))
+ax2.set_title("Read Quality of SNPs",size=15)
+ax2.set_xlabel("Read Quality",size=10)
+ax2.set_ylabel("Postion of SNP",size=10)
 
-ax[2].hist(frequency, bins = 50,density = True)
-ax[2].set_title("Allele Frequency of SNPs")
-ax[2].set_xlabel("Allele Frequency")
-ax[2].set_ylabel("Postion of SNP")
+ax3.hist(frequency, bins = 50,density = True)
+ax3.set_title("Allele Frequency of SNPs", size =15)
+ax3.set_xlabel("Allele Frequency",size=10)
+for tick in ax3.get_xticklabels():
+    tick.set_rotation(90)
+    tick.set_size(8)
+ax3.set_ylabel("Postion of SNP",size=10)
 
 plt.bar(range(len(ai)),list(ai.values()), align = 'center')
-plt.xticks(range(len(ai)),list(ai.keys()), rotation= 'vertical', size=6)
-ax[3].set_title("Allele Frequency of SNPs")
-ax[3].set_xlabel("Allele Frequency")
-ax[3].set_ylabel("Postion of SNP")
+plt.xticks(range(len(ai)),list(ai.keys()), rotation= "vertical", size=8)
+ax4.set_title("Allele Frequency of SNPs", size=15)
+ax4.set_xlabel("Allele Frequency",size=10)
+ax4.set_ylabel("Postion of SNP",size=10)
 
-
-# plt.tight_layout()
-fig.savefig("plot1.png")
+fig.set_size_inches(40, 14)
+plt.tight_layout()
+fig.savefig("THEPLOT.png",dpi=200)
 plt.close(fig)
